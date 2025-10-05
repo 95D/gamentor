3b85e97a3c174cc54d5d8717373a3bdb568783d1
95D <avos170@gmail.com>
2025-10-06 02:49:32 +0900

    Create domain model MessagePageAnchor

---
+++ library-modules/chat-message-domain-api/src/main/java/jp/co/nintendo/chat/message/domain/model/paging/MessagePageAnchor.kt
@@ -0,0 +1,16 @@
+package jp.co.nintendo.chat.message.domain.model.paging
+
+/**
+ * Anchor data that serves as a reference for messages loaded per page by paging
+ */
+sealed interface MessagePageAnchor {
+    /**
+     * A [MessagePageAnchor] for latest messages
+     */
+    data object Latest : MessagePageAnchor
+
+    /**
+     * A [MessagePageAnchor] for messages around message with messageId
+     */
+    data class Around(val messageId: String) : MessagePageAnchor
+}

0cefffe3b75d4e86d763dc032b9b3e0495255763
95D <avos170@gmail.com>
2025-10-08 00:11:14 +0900

    Update ChatMessage model structure - Update content types - Separate classes In order to serialize meta data as json

--- library-modules/chat-domain-api/src/main/java/jp/co/nintendo/chat/domain/message/model/content/MessageContent.kt
+++ library-modules/chat-domain-api/src/main/java/jp/co/nintendo/chat/domain/message/model/content/MessageContent.kt
@@ -1,12 +1,60 @@
 package jp.co.nintendo.chat.domain.message.model.content
 
+import jp.co.nintendo.chat.domain.message.model.content.system.SystemMessageErrorType
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.SerialName
+
 /**
  * Represents the content payload of a chat message.
  * Supports text, tool invocations, and tool execution results.
  */
-sealed interface MessageContent {
-    /**
-     * A [MessageContent] representing plain text.
-     */
-    data class Text(val rawText: String) : MessageContent
+@Serializable
+@SerialName("message_content")
+sealed interface MessageContent
+/**
+ * A [MessageContent] representing plain text.
+ */
+@Serializable
+@SerialName("text_content")
+data class TextContent(
+    @SerialName("raw_text")
+    val rawText: String
+) : MessageContent
+
+/**
+ * A [MessageContent] representing a request to invoke a tool with specified arguments.
+ */
+@Serializable
+@SerialName("tool_request_content")
+data class ToolRequestContent(
+    val toolCalls: List<ToolCall>
+) : MessageContent {
+
+    @Serializable
+    data class ToolCall(
+        @SerialName("tool_call_id")
+        val toolCallId: String,
+        @SerialName("tool_name")
+        val toolName: String,
+        @SerialName("argument_json")
+        val argumentsJson: String
+    )
 }
+
+/**
+ * A [MessageContent] representing the result returned from a tool execution.
+ */
+@Serializable
+@SerialName("tool_return_content")
+data class ToolReturnContent(
+    @SerialName("tool_call_id")
+    val toolCallId: String,
+    val content: String
+) : MessageContent
+
+@Serializable
+@SerialName("system_error_content")
+data class SystemErrorContent(
+    @SerialName("error_type")
+    val errorType: SystemMessageErrorType
+)

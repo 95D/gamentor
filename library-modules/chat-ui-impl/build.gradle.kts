5ad6842ffced97f0d85b13c69bce49dc31073a6f
95D <avos170@gmail.com>
2025-10-10 00:49:43 +0900

    Create chat-ui-impl module

---
+++ library-modules/chat-ui-impl/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
+    id("jp.co.nintendo.android.library")
+}
+
+android {
+    namespace = "jp.co.nintendo.chat.ui.impl"
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    implementation(project(":library-modules:chat-ui-api"))
+    implementation(project(":library-modules:chat-domain-api"))
+    implementation(project(":library-modules:automation-domain-api"))
+    implementation(project(":library-modules:multi-lang-resources"))
+    implementation(project(":library-modules:design-system"))
+    ksp(libs.hilt.compiler)
+    implementation(libs.hilt.android)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.timber)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.bundles.compose)
+    debugImplementation(libs.bundles.compose.debug)
+    implementation(libs.bundles.lifecycle)
+    implementation(libs.bundles.paging)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}

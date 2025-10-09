53f75c51f596949deb8f409c1fbfa2f98b7d3d13
95D <avos170@gmail.com>
2025-10-09 20:29:58 +0900

    Wiring DI for ChatDataRepository module

---
+++ library-modules/chat-data-repository-impl/src/main/java/jp/co/nintendo/chat/data/repository/impl/di/ChatDataRepositoryBindings.kt
@@ -0,0 +1,27 @@
+package jp.co.nintendo.chat.data.repository.impl.di
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import jp.co.nintendo.chat.data.repository.impl.channel.repository.ChatChannelRepositoryImpl
+import jp.co.nintendo.chat.data.repository.impl.message.assistant.repository.AiAssistantChatRepositoryImpl
+import jp.co.nintendo.chat.domain.channel.repository.ChatChannelRepository
+import jp.co.nintendo.chat.domain.message.repository.ChatMessageRepository
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class ChatDataRepositoryBindings {
+    @Binds
+    @Singleton
+    abstract fun bindAiAssistantChatRepository(
+        impl: AiAssistantChatRepositoryImpl
+    ): ChatMessageRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindChatChannelRepository(
+        impl: ChatChannelRepositoryImpl
+    ): ChatChannelRepository
+}

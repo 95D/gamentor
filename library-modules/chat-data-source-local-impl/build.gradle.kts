d81037fbdb827f898e85b99d8f8f8ded2b6c9fb3
95D <avos170@gmail.com>
2025-10-09 21:40:40 +0900

    Create chat-data-source-local-impl module

---
+++ library-modules/chat-data-source-local-impl/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.ksp)
+    id("jp.co.nintendo.android.library")
+}
+
+android {
+    namespace = "jp.co.nintendo.chat.data.source.local.impl"
+}
+
+dependencies {
+
+    implementation(project(":library-modules:chat-domain-api"))
+    implementation(project(":library-modules:chat-data-source-local-api"))
+    ksp(libs.hilt.compiler)
+    implementation(libs.hilt.android)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.bundles.room)
+    implementation(libs.timber)
+    ksp(libs.androidx.room.compiler)
+    implementation(libs.bundles.coroutines)
+    implementation(libs.bundles.paging)
+    implementation(libs.kotlinx.serialization.json)
+
+    testImplementation(libs.bundles.testing)
+    androidTestImplementation(libs.androidx.junit)
+    testImplementation(libs.androidx.room.testing)
+}
+

05bcec5546fd142179bcba472c9ebd8be9fa519e
95D <avos170@gmail.com>
2025-10-09 22:51:11 +0900

    Create ChatMessageMapper

---
+++ library-modules/chat-data-repository-impl/src/main/java/jp/co/nintendo/chat/data/repository/impl/message/mapper/ChatMessageMapper.kt
@@ -0,0 +1,21 @@
+package jp.co.nintendo.chat.data.repository.impl.message.mapper
+
+import jp.co.nintendo.chat.data.repository.impl.di.ChatDataRepositoryCommon
+import jp.co.nintendo.chat.data.source.local.message.entity.ChatMessageEntity
+import jp.co.nintendo.chat.domain.message.model.ChatMessage
+import kotlinx.serialization.json.Json
+import javax.inject.Inject
+
+/**
+ * A mapper class for mapping data models to domain model [ChatMessage]
+ */
+class ChatMessageMapper @Inject constructor(
+    @param:ChatDataRepositoryCommon private val json: Json
+) {
+    fun mapToDomain(entity: ChatMessageEntity): ChatMessage = ChatMessage(
+        localMessageId = entity.localMessageId,
+        createdAtMillis = entity.createdAtMillis,
+        content = json.decodeFromString(entity.contentJson),
+        senderExtras = json.decodeFromString(entity.senderExtrasJson)
+    )
+}

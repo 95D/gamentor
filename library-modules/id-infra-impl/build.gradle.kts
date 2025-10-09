bc9335c18aee25ee160e7abf151b27d76776cf89
95D <avos170@gmail.com>
2025-10-09 13:31:57 +0900

    Update dependencies in id-infra-impl module

--- library-modules/id-infra-impl/build.gradle.kts
+++ library-modules/id-infra-impl/build.gradle.kts
@@ -11,11 +11,13 @@ android {
 }
 
 dependencies {
+    implementation(project(":library-modules:id-domain-api"))
     ksp(libs.hilt.compiler)
+    implementation(libs.hilt.android)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    testImplementation(libs.junit)
+    testImplementation(libs.bundles.testing)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }

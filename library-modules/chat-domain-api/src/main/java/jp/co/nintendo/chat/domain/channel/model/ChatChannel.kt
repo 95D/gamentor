ca16d555e0d35858621e9e8864356e13ad3ab3f9
viento <avos170@gmail.com>
2025-10-11 07:46:53 +0900

    Create ChatChannel model

---
+++ library-modules/chat-domain-api/src/main/java/jp/co/nintendo/chat/domain/channel/model/ChatChannel.kt
@@ -0,0 +1,9 @@
+package jp.co.nintendo.chat.domain.channel.model
+
+/**
+ * A model representing chat channel
+ */
+data class ChatChannel(
+    val channelId: String,
+    val displayName: String
+)

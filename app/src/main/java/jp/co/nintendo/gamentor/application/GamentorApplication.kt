e54eb553cca66a0c2a462386578be53bd834e97c
viento <avos170@gmail.com>
2025-10-11 09:06:39 +0900

    Create application class

---
+++ app/src/main/java/jp/co/nintendo/gamentor/application/GamentorApplication.kt
@@ -0,0 +1,16 @@
+package jp.co.nintendo.gamentor.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+/**
+ * A product [Application] class for this App
+ */
+@HiltAndroidApp
+class GamentorApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}

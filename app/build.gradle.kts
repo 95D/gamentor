45277d249ed0b39039452502759dfbbbfaad80ef
viento <avos170@gmail.com>
2025-10-11 09:08:00 +0900

    Update build script of app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,9 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.ksp)
     id("jp.co.nintendo.android.application")
 }
 
@@ -15,8 +16,17 @@ android {
 }
 
 dependencies {
+    implementation(project(":library-modules:chat-ui-api"))
+    implementation(project(":library-modules:design-system"))
+    implementation(project(":library-modules:app-di"))
+    ksp(libs.hilt.compiler)
+    implementation(libs.hilt.android)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
+    implementation(libs.timber)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.bundles.compose)
+    debugImplementation(libs.bundles.compose.debug)
     implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)

47e0b1f6383c32877d3c2f4338b8e38f46bc2a6a
viento <avos170@gmail.com>
2025-10-13 23:50:24 +0900

    Update ChatChannelRepository
    - Update insert method

--- library-modules/chat-data-repository-impl/src/main/java/jp/co/nintendo/chat/data/repository/impl/channel/repository/ChatChannelRepositoryImpl.kt
+++ library-modules/chat-data-repository-impl/src/main/java/jp/co/nintendo/chat/data/repository/impl/channel/repository/ChatChannelRepositoryImpl.kt
@@ -3,8 +3,10 @@ package jp.co.nintendo.chat.data.repository.impl.channel.repository
 import androidx.paging.PagingData
 import androidx.paging.map
 import jakarta.inject.Inject
+import jp.co.nintendo.chat.data.repository.impl.channel.factory.ChatChannelEntityFactory
 import jp.co.nintendo.chat.data.repository.impl.channel.mapper.ChatChannelMapper
 import jp.co.nintendo.chat.data.source.local.channel.ChatChannelLocalDataSource
+import jp.co.nintendo.chat.data.source.local.channel.model.ChatChannelInsertResult
 import jp.co.nintendo.chat.domain.channel.model.ChatChannel
 import jp.co.nintendo.chat.domain.channel.repository.ChatChannelRepository
 import kotlinx.coroutines.flow.Flow
@@ -15,11 +17,24 @@ import kotlinx.coroutines.flow.map
  */
 class ChatChannelRepositoryImpl @Inject constructor(
     private val chatChannelLocalDataSource: ChatChannelLocalDataSource,
-    private val chatChannelMapper: ChatChannelMapper
+    private val chatChannelMapper: ChatChannelMapper,
+    private val chatChannelEntityFactory: ChatChannelEntityFactory
 ) : ChatChannelRepository {
-    override suspend fun loadChannelPage(): Flow<PagingData<ChatChannel>> =
+    override fun loadChannelPage(): Flow<PagingData<ChatChannel>> =
         chatChannelLocalDataSource.selectChannelPagingSource(0)
             .map { it.map(chatChannelMapper::mapToDomain) }
+
+    override suspend fun createNewChatChannel(): String? {
+        val entity = chatChannelEntityFactory.create()
+        val insertResult = chatChannelLocalDataSource.insert(
+            entity = entity
+        )
+        return when (insertResult) {
+            ChatChannelInsertResult.Failure.FullDisk,
+            ChatChannelInsertResult.Failure.Unknown -> null
+            ChatChannelInsertResult.Success -> entity.channelId
+        }
+    }
 
     override suspend fun selectChannel(channelId: String): ChatChannel? =
         chatChannelLocalDataSource.selectChannel(channelId)

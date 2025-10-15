e37773230eb974270b66a4f721da605a708d6485
viento <avos170@gmail.com>
2025-10-16 04:38:44 +0900

    Update automation-domain-api module

---
+++ library-modules/automation-domain-api/src/main/java/jp/co/nintendo/automation/domain/tool/model/decision/UserApproveLabel.kt
@@ -0,0 +1,8 @@
+package jp.co.nintendo.automation.domain.tool.model.decision
+
+/**
+ * An enum class representing text label type of [UserDecision.Approve]
+ */
+enum class UserApproveLabel {
+    READ_GAME_DATA
+}

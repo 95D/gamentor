7bc0efa7d6f7f8e6889a4bf557ac13425070361a
viento <avos170@gmail.com>
2025-10-16 04:42:37 +0900

    Update theme system

--- library-modules/design-system/src/main/java/jp/co/nintendo/design/system/colors/SemanticColors.kt
+++ library-modules/design-system/src/main/java/jp/co/nintendo/design/system/colors/SemanticColors.kt
@@ -6,6 +6,7 @@ import androidx.compose.ui.graphics.Color
 @Immutable
 data class SemanticColors(
     val surfacePrimary: Color,
+    val surfaceSecondary: Color,
     val header: Color,
     val headerText: Color,
     val textDefault: Color,
@@ -14,40 +15,57 @@ data class SemanticColors(
     val buttonPrimary: Color,
     val background: Color,
     val backgroundText: Color,
+    val messageBubbleOutlineNone: Color,
+    val messageBubbleOutlineOthers: Color,
     val messageBubbleMine: Color,
     val messageBubbleOthers: Color,
-    val messageBubbleOthersOutline: Color,
-    val messageBubbleText: Color
+    val messageBubbleSystem: Color,
+    val messageBubbleDeveloper: Color,
+    val messageBubbleTextNormal: Color,
+    val messageBubbleTextDeveloper: Color,
+    val messageBubbleTextSystem: Color
 )
 
 val LightSemanticColors = SemanticColors(
     surfacePrimary = GrayF0Alpha100,
-    header = NintendoRed,
-    headerText = GrayFAAlpha100,
+    surfaceSecondary = GrayE6Alpha100,
+    header = Transparent,
+    headerText = Gray30Alpha100,
     textDefault = Gray30Alpha100,
     textAccent = AccentSkyBlue,
     buttonPrimary = NintendoRed,
     buttonPrimaryText = GrayFAAlpha100,
     background = Gray98Alpha100,
     backgroundText = Gray30Alpha100,
+    messageBubbleOutlineNone = Transparent,
+    messageBubbleOutlineOthers = DarkGray,
     messageBubbleMine = AccentSkyRed,
     messageBubbleOthers = GrayE6Alpha100,
-    messageBubbleOthersOutline = DarkGray,
-    messageBubbleText = DarkGray
+    messageBubbleSystem = Gray20Alpha72,
+    messageBubbleDeveloper = AccentSkyBlue,
+    messageBubbleTextNormal = Gray12Alpha100,
+    messageBubbleTextDeveloper = AccentSkyRed,
+    messageBubbleTextSystem = GrayFAAlpha100
 )
 
 val DarkSemanticColors = SemanticColors(
     surfacePrimary = Gray12Alpha100,
-    header = NintendoRed,
-    headerText = Gray30Alpha100,
+    surfaceSecondary = Gray30Alpha100,
+    header = Transparent,
+    headerText = GrayFAAlpha100,
     textDefault = GrayFAAlpha100,
     textAccent = AccentSkyBlue,
     buttonPrimaryText = GrayFAAlpha100,
     buttonPrimary = NintendoRed,
     background = DarkGray,
     backgroundText = Gray98Alpha100,
+    messageBubbleOutlineNone = Transparent,
+    messageBubbleOutlineOthers = DarkGray,
     messageBubbleMine = AccentSkyRed,
     messageBubbleOthers = GrayE6Alpha100,
-    messageBubbleOthersOutline = DarkGray,
-    messageBubbleText = DarkGray
+    messageBubbleSystem = Gray20Alpha72,
+    messageBubbleDeveloper = AccentSkyBlue,
+    messageBubbleTextNormal = Gray12Alpha100,
+    messageBubbleTextDeveloper = AccentSkyRed,
+    messageBubbleTextSystem = GrayFAAlpha100
 )

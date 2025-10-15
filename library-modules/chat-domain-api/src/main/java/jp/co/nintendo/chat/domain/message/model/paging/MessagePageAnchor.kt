b600c29bee5f261c31d67d6b55500eef080a74a4
viento <avos170@gmail.com>
2025-10-16 04:43:23 +0900

    Update MessagePageAnchor

--- library-modules/chat-domain-api/src/main/java/jp/co/nintendo/chat/domain/message/model/paging/MessagePageAnchor.kt
+++ library-modules/chat-domain-api/src/main/java/jp/co/nintendo/chat/domain/message/model/paging/MessagePageAnchor.kt
@@ -4,13 +4,16 @@ package jp.co.nintendo.chat.domain.message.model.paging
  * Anchor data that serves as a reference for messages loaded per page by paging
  */
 sealed interface MessagePageAnchor {
+    val channelId: String
+
     /**
      * A [MessagePageAnchor] for latest messages
      */
-    data object Latest : MessagePageAnchor
+    data class Latest(override val channelId: String) : MessagePageAnchor
 
     /**
      * A [MessagePageAnchor] for messages around message with messageId
      */
-    data class Around(val localMessageId: String) : MessagePageAnchor
+    data class Around(override val channelId: String, val localMessageId: String) :
+        MessagePageAnchor
 }

9eb9980333624d5df626fa294022745b7d9e4e67
viento <avos170@gmail.com>
2025-10-16 04:42:21 +0900

    Update build script of design-system

--- library-modules/design-system/build.gradle.kts
+++ library-modules/design-system/build.gradle.kts
@@ -12,12 +12,14 @@ android {
 }
 
 dependencies {
+    implementation(project(":library-modules:multi-lang-resources"))
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.bundles.compose)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.bundles.compose)
+    implementation(libs.bundles.adaptive.layout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)

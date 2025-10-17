e5b4b4a141a50285fe32cc4d2fc418f0268fa44d
viento <avos170@gmail.com>
2025-10-18 00:49:29 +0900

    Remove unnecessary screen preview

--- library-modules/chat-ui-impl/src/main/java/jp/co/nintendo/chat/ui/impl/chatlist/compose/ChatListScreen.kt
+++ library-modules/chat-ui-impl/src/main/java/jp/co/nintendo/chat/ui/impl/chatlist/compose/ChatListScreen.kt
@@ -19,7 +19,6 @@ import androidx.compose.material.icons.automirrored.filled.Chat
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -29,7 +28,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.compose.collectAsLazyPagingItems
@@ -155,17 +153,4 @@ fun ChatListScreenContent(
         onDismissBottomSheet = chatListViewModel::dismissBottomSheet,
         onSelectChannelContextAction = chatListViewModel::selectChannelContextAction
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun PreviewChatListScreen() {
-    MaterialTheme {
-        ChatListScreen(
-            isExpandedScreen = false,
-            selectedChannelName = null,
-            onNavigateToChatChannel = {},
-            onBackClicked = {}
-        )
-    }
 }

57654c9015107bdf04ef11fdf8bc2402fbc292dc
viento <avos170@gmail.com>
2025-10-18 00:40:56 +0900

    Improve NdsListScreenAppBar

--- library-modules/design-system/src/main/java/jp/co/nintendo/design/system/ui/NdsListScreenAppBar.kt
+++ library-modules/design-system/src/main/java/jp/co/nintendo/design/system/ui/NdsListScreenAppBar.kt
@@ -2,11 +2,9 @@ package jp.co.nintendo.design.system.ui
 
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ContentTransform
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
-import androidx.compose.animation.slideInHorizontally
-import androidx.compose.animation.slideOutHorizontally
-import androidx.compose.animation.togetherWith
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -16,8 +14,11 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import jp.co.nintendo.design.system.animation.ndsNoneTransform
+import jp.co.nintendo.design.system.animation.ndsTargetFadeInContentTransform
 import jp.co.nintendo.design.system.theme.AppTheme
 import jp.co.nintendo.design.system.theme.LocalAppSemanticColors
 import jp.co.nintendo.multi.lang.resources.R as MultiLangR
@@ -25,51 +26,59 @@ import jp.co.nintendo.multi.lang.resources.R as MultiLangR
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NdsListScreenAppBar(
+    isExpandedScreen: Boolean,
     title: String,
     onBackClicked: () -> Unit,
     isDetailSelected: Boolean
 ) {
     val semanticColors = LocalAppSemanticColors.current
-    TopAppBar(
-        colors = TopAppBarDefaults.topAppBarColors(
-            containerColor = semanticColors.header,
-            titleContentColor = semanticColors.headerText
-        ),
-        title = {
-            AnimatedContent(
-                targetState = title,
-                transitionSpec = {
-                    (slideInHorizontally { fullWidth -> fullWidth } + fadeIn())
-                        .togetherWith(slideOutHorizontally { fullWidth -> -fullWidth } + fadeOut())
-                },
-                label = "Title change"
-            ) { targetTitle ->
-                Text(targetTitle)
-            }
-        },
-        navigationIcon = {
-            AnimatedVisibility(
-                visible = isDetailSelected,
-                enter = fadeIn(),
-                exit = fadeOut()
-            ) {
-                IconButton(onClick = onBackClicked) {
-                    Icon(
-                        Icons.AutoMirrored.Filled.ArrowBack,
-                        contentDescription =
-                            stringResource(MultiLangR.string.content_description_back_action)
-                    )
+    val titleTransitionSpec = remember(isExpandedScreen) {
+        getTitleTransitionSpec(isExpandedScreen)
+    }
+
+    AnimatedContent(
+        targetState = title,
+        transitionSpec = { titleTransitionSpec },
+        label = "Title change"
+    ) { targetTitle ->
+        TopAppBar(
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = semanticColors.header,
+                titleContentColor = semanticColors.headerText
+            ),
+            title = { Text(targetTitle) },
+            navigationIcon = {
+                AnimatedVisibility(
+                    visible = isDetailSelected,
+                    enter = fadeIn(),
+                    exit = fadeOut()
+                ) {
+                    IconButton(onClick = onBackClicked) {
+                        Icon(
+                            Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription =
+                                stringResource(MultiLangR.string.content_description_back_action)
+                        )
+                    }
                 }
             }
-        }
-    )
+        )
+    }
 }
+
+private fun getTitleTransitionSpec(isExpandedScreen: Boolean): ContentTransform =
+    if (isExpandedScreen) {
+        ndsTargetFadeInContentTransform
+    } else {
+        ndsNoneTransform
+    }
 
 @Preview(showBackground = true)
 @Composable
 fun PreviewNdsListScreenAppBar() {
     AppTheme(isDarkTheme = false) {
         NdsListScreenAppBar(
+            isExpandedScreen = false,
             title = "Test app bar",
             onBackClicked = { /* Do nothing */ },
             isDetailSelected = false

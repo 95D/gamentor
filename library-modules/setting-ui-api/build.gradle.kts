2d415f01015ee25357cbdf912143f938fb1badb5
viento <avos170@gmail.com>
2025-10-19 15:50:47 +0900

    Update build scripts of setting-ui modules

--- library-modules/setting-ui-api/build.gradle.kts
+++ library-modules/setting-ui-api/build.gradle.kts
@@ -1,7 +1,9 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.compose.compiler)
     alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.ksp)
     id("jp.co.nintendo.android.library")
 }
 
@@ -10,9 +12,13 @@ android {
 }
 
 dependencies {
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.bundles.compose)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.bundles.compose)
+    implementation(libs.bundles.adaptive.layout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)

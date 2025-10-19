f1e5cf943ce2671417d6ff6e4493e7206ffc9c3a
viento <avos170@gmail.com>
2025-10-19 20:05:07 +0900

    Define custom semantic color for user theme Sakura

---
+++ library-modules/design-system/src/main/java/jp/co/nintendo/design/system/colors/SakuraAppColors.kt
@@ -0,0 +1,7 @@
+package jp.co.nintendo.design.system.colors
+
+import androidx.compose.ui.graphics.Color
+
+val SakuraLightPink: Color = Color(0xFFFFE4E6)
+val SakuraPink: Color = Color(0xFFFDC0C7)
+val SakuraDeepPink: Color = Color(0xFFE9A0B8)

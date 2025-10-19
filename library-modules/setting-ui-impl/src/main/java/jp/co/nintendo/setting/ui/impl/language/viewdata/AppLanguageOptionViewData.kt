e57214fdd354fec4421e7f9e5f6e1b4fd829d96e
viento <avos170@gmail.com>
2025-10-19 15:53:02 +0900

    Create view data classes for app language setting detail screen

---
+++ library-modules/setting-ui-impl/src/main/java/jp/co/nintendo/setting/ui/impl/language/viewdata/AppLanguageOptionViewData.kt
@@ -0,0 +1,9 @@
+package jp.co.nintendo.setting.ui.impl.language.viewdata
+
+/**
+ * A view data representing setting item of App language setting
+ */
+data class AppLanguageOptionViewData(
+    val isSelected: Boolean,
+    val languageViewType: AppLanguageOptionViewType
+)
---
+++ library-modules/setting-ui-impl/src/main/java/jp/co/nintendo/setting/ui/impl/language/viewdata/AppLanguageOptionViewType.kt
@@ -0,0 +1,26 @@
+package jp.co.nintendo.setting.ui.impl.language.viewdata
+
+import androidx.annotation.StringRes
+import java.util.Locale
+import jp.co.nintendo.multi.lang.resources.R as MultiLangR
+
+/**
+ * An enum class representing view type of App language option
+ */
+enum class AppLanguageOptionViewType(
+    @param:StringRes val displayName: Int,
+    val languageTag: String
+) {
+    DEFAULT(displayName = MultiLangR.string.locale_default, languageTag = ""),
+    ENGLISH(displayName = MultiLangR.string.locale_english, languageTag = Locale.ENGLISH.language),
+    JAPANESE(
+        displayName = MultiLangR.string.locale_japanese,
+        languageTag = Locale.JAPANESE.language
+    );
+
+    companion object {
+        fun from(languageTag: String): AppLanguageOptionViewType = entries.firstOrNull {
+            it.languageTag == languageTag
+        } ?: DEFAULT
+    }
+}

0e23217fb5f965589a4cde5f2986ba002c71606b
viento <avos170@gmail.com>
2025-10-19 15:53:27 +0900

    Create AppLanguageSettingViewModel

---
+++ library-modules/setting-ui-impl/src/main/java/jp/co/nintendo/setting/ui/impl/language/viewmodel/AppLanguageSettingViewModel.kt
@@ -0,0 +1,47 @@
+package jp.co.nintendo.setting.ui.impl.language.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import jakarta.inject.Inject
+import jp.co.nintendo.setting.language.adapter.AppLanguageSettingAdapter
+import jp.co.nintendo.setting.ui.impl.language.viewdata.AppLanguageOptionViewData
+import jp.co.nintendo.setting.ui.impl.language.viewdata.AppLanguageOptionViewType
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+/**
+ * A view model class for managing state in app language setting screen
+ */
+@HiltViewModel
+class AppLanguageSettingViewModel @Inject constructor(
+    private val appLanguageSettingAdapter: AppLanguageSettingAdapter
+) : ViewModel() {
+    private val currentLanguageMutableStateFlow: MutableStateFlow<AppLanguageOptionViewType> =
+        MutableStateFlow(getCurrentAppLanguageType())
+
+    val appLanguagesStateFlow: StateFlow<List<AppLanguageOptionViewData>> =
+        currentLanguageMutableStateFlow.map { selectedLanguage ->
+            AppLanguageOptionViewType.entries.map {
+                AppLanguageOptionViewData(
+                    isSelected = it == selectedLanguage,
+                    languageViewType = it
+                )
+            }.toList()
+        }.stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.Eagerly,
+            initialValue = emptyList()
+        )
+
+    fun selectAppLanguage(appLanguageOptionViewType: AppLanguageOptionViewType) {
+        appLanguageSettingAdapter.setAppLanguageTag(appLanguageOptionViewType.languageTag)
+        currentLanguageMutableStateFlow.value = getCurrentAppLanguageType()
+    }
+
+    private fun getCurrentAppLanguageType(): AppLanguageOptionViewType =
+        AppLanguageOptionViewType.from(appLanguageSettingAdapter.getCurrentLanguageTag())
+}

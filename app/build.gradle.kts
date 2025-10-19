3ea5f05742042d1037da35c0589cb9788d59e3a2
viento <avos170@gmail.com>
2025-10-19 20:02:56 +0900

    Import setting-domain-api module into app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,7 @@ android {
 dependencies {
     implementation(project(":library-modules:chat-ui-api"))
     implementation(project(":library-modules:setting-ui-api"))
+    implementation(project(":library-modules:setting-domain-api"))
     implementation(project(":library-modules:design-system"))
     implementation(project(":library-modules:app-di"))
     ksp(libs.hilt.compiler)

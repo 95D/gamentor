234bed787443f237148b69ba12a237e7ee57017b
viento <avos170@gmail.com>
2025-10-19 15:51:54 +0900

    Create view data classes for app setting screen

---
+++ library-modules/setting-ui-impl/src/main/java/jp/co/nintendo/setting/ui/impl/app/viewdata/AppSettingDetailContentKey.kt
@@ -0,0 +1,10 @@
+package jp.co.nintendo.setting.ui.impl.app.viewdata
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+/**
+ * A content key for navigating detail setting page of [AppSettingDetailItemType]
+ */
+@Parcelize
+data class AppSettingDetailContentKey(val itemType: AppSettingDetailItemType): Parcelable
---
+++ library-modules/setting-ui-impl/src/main/java/jp/co/nintendo/setting/ui/impl/app/viewdata/AppSettingDetailItemType.kt
@@ -0,0 +1,17 @@
+package jp.co.nintendo.setting.ui.impl.app.viewdata
+
+import android.os.Parcelable
+import androidx.annotation.StringRes
+import kotlinx.parcelize.Parcelize
+import jp.co.nintendo.multi.lang.resources.R as MultiLangR
+
+/**
+ * An enum class representing app setting item
+ */
+@Parcelize
+enum class AppSettingDetailItemType(
+    @param:StringRes val settingItemName: Int
+) : Parcelable {
+    APP_LANGUAGE(settingItemName = MultiLangR.string.setting_item_app_language),
+    APP_THEME(settingItemName = MultiLangR.string.setting_item_app_theme)
+}

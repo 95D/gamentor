7a424699f91507a0f5d9ea3268aca5e7e5a10658
viento <avos170@gmail.com>
2025-10-20 00:36:34 +0900

    Activate edgeToEdge for low SDK levels

--- app/src/main/java/jp/co/nintendo/gamentor/MainActivity.kt
+++ app/src/main/java/jp/co/nintendo/gamentor/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
@@ -39,6 +40,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             App(chatListEntry, appSettingEntry)
         }

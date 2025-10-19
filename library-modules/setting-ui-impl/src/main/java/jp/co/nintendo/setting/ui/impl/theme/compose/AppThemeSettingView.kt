6e3d5fcafe7fa351d94997d479d2fe03557dc318
viento <avos170@gmail.com>
2025-10-19 15:56:56 +0900

    Create AppThemeSettingView

---
+++ library-modules/setting-ui-impl/src/main/java/jp/co/nintendo/setting/ui/impl/theme/compose/AppThemeSettingView.kt
@@ -0,0 +1,31 @@
+package jp.co.nintendo.setting.ui.impl.theme.compose
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import jp.co.nintendo.setting.ui.impl.theme.viewmodel.AppThemeSettingViewModel
+
+
+@Composable
+fun AppThemeSettingView(
+    modifier: Modifier = Modifier,
+    appThemeSettingViewModel: AppThemeSettingViewModel = hiltViewModel()
+) {
+    val appThemes by appThemeSettingViewModel.appThemesStateFlow
+        .collectAsState()
+
+    LazyColumn(modifier = modifier) {
+        items(appThemes) { item ->
+            AppThemeSettingOptionRow(
+                viewData = item,
+                onClickItem = { themeViewType ->
+                    appThemeSettingViewModel.selectTheme(themeViewType)
+                }
+            )
+        }
+    }
+}
